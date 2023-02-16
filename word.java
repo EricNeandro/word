5cae28d97e0cdfd92b36a4b00d79728e0afac61f
EricNeandro <ericneandro1999@gmail.com>
2023-02-15 21:57:42 -0300

    Enviando meu primeiro controle de versão

---
+++ word.java
@@ -0,0 +1,9 @@
+public class MyFistHelloWord {
+
+    public satic void main (String [] args) {
+        //TODO Auto-generated method stub
+        System.out.println ("Meu primeiro hello word!!!);
+
+    }
+
+}
